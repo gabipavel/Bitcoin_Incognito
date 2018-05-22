c7bf1916e4cb92587dbe075ff49e95bc5523b60c
Bit Yoda <36884085+Realbityoda@users.noreply.github.com>
2018-05-22 02:15:21 -0700

    Update btci_install.sh

--- btci_install.sh
+++ btci_install.sh
@@ -1,19 +1,18 @@
-#!/bin/bash
-
-TMP_FOLDER=$(mktemp -d)
-CONFIG_FILE='btci.conf'
-CONFIGFOLDER='/root/.BTCi'
-COIN_DAEMON='btcid'
-COIN_CLI='btci-cli'
-COIN_PATH='/usr/local/bin/'
-COIN_TGZ='https://github.com/Realbityoda/Bitcoin_Incognito/releases/download/1.0.0.0/BTCi.tar.gz'
-COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
-COIN_NAME='BTCi'
-COIN_PORT=7250
-RPC_PORT=7249
-
-NODEIP=$(curl -s4 icanhazip.com)
-
+-#!/bin/bash
+-
+-TMP_FOLDER=$(mktemp -d)
+-CONFIG_FILE='btci.conf'
+-CONFIGFOLDER='/root/.BTCi'
+-COIN_DAEMON='/usr/local/bin/btcid'
+-COIN_CLI='/usr/local/bin/btci-cli'
+-COIN_REPO='https://github.com/Realbityoda/Bitcoin_Incognito/releases/download/v1.0.0.0/btci.tar.gz'
+-COIN_NAME='BTCi'
+-COIN_PORT=7250
+-RPC_PORT=7249
+-
+-
+-NODEIP=$(curl -s4 icanhazip.com)
+-
 BLUE="\033[0;34m"
 YELLOW="\033[0;33m"
 CYAN="\033[0;36m" 
@@ -22,7 +21,7 @@ RED='\033[0;31m'
 GREEN="\033[0;32m"
 NC='\033[0m'
 MAG='\e[1;35m'
-
+-
 purgeOldInstallation() {
     echo -e "${GREEN}Searching and removing old $COIN_NAME files and configurations${NC}"
     #kill wallet daemon
@@ -37,209 +36,257 @@ purgeOldInstallation() {
     cd /usr/local/bin && sudo rm btci-cli btcid-tx btcid > /dev/null 2>&1 && cd
     echo -e "${GREEN}* Done${NONE}";
 }
-
-
-function download_node() {
-  echo -e "${GREEN}Downloading and Installing VPS $COIN_NAME Daemon${NC}"
-  cd $TMP_FOLDER >/dev/null 2>&1
-  rm $COIN_ZIP >/dev/null 2>&1
-  wget -q $COIN_TGZ
-  compile_error
-  tar xvzf $COIN_ZIP >/dev/null 2>&1
-  chmod +x $COIN_DAEMON $COIN_CLI
-  compile_error
-  cp $COIN_DAEMON $COIN_CLI $COIN_PATH
-  cd - >/dev/null 2>&1
-  rm -rf $TMP_FOLDER >/dev/null 2>&1
-  clear
-}
-
-function configure_systemd() {
-  cat << EOF > /etc/systemd/system/$COIN_NAME.service
-[Unit]
-Description=$COIN_NAME service
-After=network.target
-
-[Service]
-User=root
-Group=root
-
-Type=forking
-#PIDFile=$CONFIGFOLDER/$COIN_NAME.pid
-
-ExecStart=$COIN_PATH$COIN_DAEMON -daemon -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER
-ExecStop=-$COIN_PATH$COIN_CLI -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER stop
-
-Restart=always
-PrivateTmp=true
-TimeoutStopSec=60s
-TimeoutStartSec=10s
-StartLimitInterval=120s
-StartLimitBurst=5
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-  systemctl daemon-reload
-  sleep 3
-  systemctl start $COIN_NAME.service
-  systemctl enable $COIN_NAME.service >/dev/null 2>&1
-
-  if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
-    echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
-    echo -e "${GREEN}systemctl start $COIN_NAME.service"
-    echo -e "systemctl status $COIN_NAME.service"
-    echo -e "less /var/log/syslog${NC}"
-    exit 1
-  fi
-}
-
-
-function create_config() {
-  mkdir $CONFIGFOLDER >/dev/null 2>&1
-  RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
-  RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1)
-  cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
-rpcuser=$RPCUSER
-rpcpassword=$RPCPASSWORD
-rpcport=$RPC_PORT
-rpcallowip=127.0.0.1
-listen=1
-server=1
-daemon=1
-port=$COIN_PORT
-EOF
-}
-
-function create_key() {
-  echo -e "${YELLOW}Enter your ${RED}$COIN_NAME Masternode GEN Key${NC}."
-  read -e COINKEY
-  if [[ -z "$COINKEY" ]]; then
-  $COIN_PATH$COIN_DAEMON -daemon
-  sleep 30
-  if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
-   echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
-   exit 1
-  fi
-  COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
-  if [ "$?" -gt "0" ];
-    then
-    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the GEN Key${NC}"
-    sleep 30
-    COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
-  fi
-  $COIN_PATH$COIN_CLI stop
-fi
-clear
-}
-
-function update_config() {
-  sed -i 's/daemon=1/daemon=0/' $CONFIGFOLDER/$CONFIG_FILE
-  cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
-logintimestamps=1
-maxconnections=256
-#bind=$NODEIP
-masternode=1
-externalip=$NODEIP:$COIN_PORT
-masternodeprivkey=$COINKEY
-
-#ADDNODES
-
-EOF
-}
-
-
-function enable_firewall() {
-  echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
-  ufw allow $COIN_PORT/tcp comment "$COIN_NAME MN port" >/dev/null
-  ufw allow ssh comment "SSH" >/dev/null 2>&1
-  ufw limit ssh/tcp >/dev/null 2>&1
-  ufw default allow outgoing >/dev/null 2>&1
-  echo "y" | ufw enable >/dev/null 2>&1
-}
-
-
-function get_ip() {
-  declare -a NODE_IPS
-  for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
-  do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
-  done
-
-  if [ ${#NODE_IPS[@]} -gt 1 ]
-    then
-      echo -e "${GREEN}More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
-      INDEX=0
-      for ip in "${NODE_IPS[@]}"
-      do
-        echo ${INDEX} $ip
-        let INDEX=${INDEX}+1
-      done
-      read -e choose_ip
-      NODEIP=${NODE_IPS[$choose_ip]}
-  else
-    NODEIP=${NODE_IPS[0]}
-  fi
-}
-
-
-function compile_error() {
-if [ "$?" -gt "0" ];
- then
-  echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
-  exit 1
-fi
-}
-
-
-function checks() {
-if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
-  exit 1
-fi
-
-if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}$0 must be run as root.${NC}"
-   exit 1
-fi
-
-if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "$COIN_DAEMOM" ] ; then
-  echo -e "${RED}$COIN_NAME is already installed.${NC}"
-  exit 1
-fi
-}
-
-function prepare_system() {
-echo -e "Preparing the VPS to setup. ${CYAN}$COIN_NAME${NC} ${RED}Masternode${NC}"
-apt-get update >/dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
-apt install -y software-properties-common >/dev/null 2>&1
-echo -e "${PURPLE}Adding bitcoin PPA repository"
-apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
-echo -e "Installing required packages, it may take some time to finish.${NC}"
-apt-get update >/dev/null 2>&1
-apt-get install libzmq3-dev -y >/dev/null 2>&1
-apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
-build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
-libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
-libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  libdb5.3++ unzip libzmq5 >/dev/null 2>&1
-if [ "$?" -gt "0" ];
-  then
-    echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
-    echo "apt-get update"
-    echo "apt -y install software-properties-common"
-    echo "apt-add-repository -y ppa:bitcoin/bitcoin"
-    echo "apt-get update"
-    echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
-libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
-bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev libdb5.3++ unzip libzmq5"
- exit 1
-fi
-clear
-}
-
+-
+-function compile_node() {
+-  echo -e "${GREEN}Downloading and Installing VPS $COIN_NAME Daemon${NC}"
+-  cd $TMP_FOLDER
+-  wget -q $COIN_REPO
+-  compile_error
+-  COIN_ZIP=$(echo $COIN_REPO | awk -F'/' '{print $NF}')
+-  tar xvzf $COIN_ZIP >/dev/null 2>&1
+-  compile_error
+-  cp btci* /usr/local/bin
+-  compile_error
+-  strip $COIN_DAEMON $COIN_CLI
+-  cd -
+-  rm -rf $TMP_FOLDER >/dev/null 2>&1
+-  clear
+-}
+-
+-function configure_systemd() {
+-  cat << EOF > /etc/systemd/system/$COIN_NAME.service
+-[Unit]
+-Description=$COIN_NAME service
+-After=network.target
+-
+-[Service]
+-User=root
+-Group=root
+-
+-Type=forking
+-#PIDFile=$CONFIGFOLDER/$COIN_NAME.pid
+-
+-ExecStart=$COIN_DAEMON -daemon -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER
+-ExecStop=-$COIN_CLI -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER stop
+-
+-Restart=always
+-PrivateTmp=true
+-TimeoutStopSec=60s
+-TimeoutStartSec=10s
+-StartLimitInterval=120s
+-StartLimitBurst=5
+-
+-[Install]
+-WantedBy=multi-user.target
+-EOF
+-
+-  systemctl daemon-reload
+-  sleep 3
+-  systemctl start $COIN_NAME.service
+-  systemctl enable $COIN_NAME.service >/dev/null 2>&1
+-
+-  if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
+-    echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
+-    echo -e "${GREEN}systemctl start $COIN_NAME.service"
+-    echo -e "systemctl status $COIN_NAME.service"
+-    echo -e "less /var/log/syslog${NC}"
+-    exit 1
+-  fi
+-}
+-
+-function configure_startup() {
+-  cat << EOF > /etc/init.d/$COIN_NAME
+-#! /bin/bash
+-### BEGIN INIT INFO
+-# Provides: $COIN_NAME
+-# Required-Start: $remote_fs $syslog
+-# Required-Stop: $remote_fs $syslog
+-# Default-Start: 2 3 4 5
+-# Default-Stop: 0 1 6
+-# Short-Description: $COIN_NAME
+-# Description: This file starts and stops $COIN_NAME MN server
+-#
+-### END INIT INFO
+-
+-case "\$1" in
+- start)
+-   $COIN_DAEMON -daemon
+-   sleep 5
+-   ;;
+- stop)
+-   $COIN_CLI stop
+-   ;;
+- restart)
+-   $COIN_CLI stop
+-   sleep 10
+-   $COIN_DAEMON -daemon
+-   ;;
+- *)
+-   echo "Usage: $COIN_NAME {start|stop|restart}" >&2
+-   exit 3
+-   ;;
+-esac
+-EOF
+-chmod +x /etc/init.d/$COIN_NAME >/dev/null 2>&1
+-update-rc.d $COIN_NAME defaults >/dev/null 2>&1
+-/etc/init.d/$COIN_NAME start >/dev/null 2>&1
+-if [ "$?" -gt "0" ]; then
+- sleep 5
+- /etc/init.d/$COIN_NAME start >/dev/null 2>&1
+-fi
+-}
+-
+-
+-function create_config() {
+-  mkdir $CONFIGFOLDER >/dev/null 2>&1
+-  RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
+-  RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1)
+-  cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
+-rpcuser=$RPCUSER
+-rpcpassword=$RPCPASSWORD
+-rpcallowip=127.0.0.1
+-listen=1
+-server=1
+-daemon=1
+-port=$COIN_PORT
+-EOF
+-}
+-
+-function create_key() {
+-  echo -e "${YELLOW}Enter your ${RED}$COIN_NAME Masternode GEN Key${NC}."
+-  read -e COINKEY
+-  if [[ -z "$COINKEY" ]]; then
+-  $COIN_DAEMON -daemon
+-  sleep 30
+-  if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
+-   echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
+-   exit 1
+-  fi
+-  COINKEY=$($COIN_CLI masternode genkey)
+-  if [ "$?" -gt "0" ];
+-    then
+-    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
+-    sleep 30
+-    COINKEY=$($COIN_CLI masternode genkey)
+-  fi
+-  $COIN_CLI stop
+-fi
+-clear
+-}
+-
+-function update_config() {
+-  sed -i 's/daemon=1/daemon=0/' $CONFIGFOLDER/$CONFIG_FILE
+-  cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
+-logintimestamps=1
+-maxconnections=256
+-#bind=$NODEIP
+-masternode=1
+-externalip=$NODEIP:$COIN_PORT
+-masternodeprivkey=$COINKEY
+-EOF
+-}
+-
+-
+-function enable_firewall() {
+-  echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
+-  ufw allow $COIN_PORT/tcp >/dev/null 2>&1
+-  ufw allow ssh >/dev/null 2>&1
+-  ufw limit ssh/tcp >/dev/null 2>&1
+-  ufw default allow outgoing >/dev/null 2>&1
+-  echo "y" | ufw enable >/dev/null 2>&1
+-}
+-
+-
+-
+-function get_ip() {
+-  declare -a NODE_IPS
+-  for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
+-  do
+-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+-  done
+-
+-  if [ ${#NODE_IPS[@]} -gt 1 ]
+-    then
+-      echo -e "${GREEN}More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
+-      INDEX=0
+-      for ip in "${NODE_IPS[@]}"
+-      do
+-        echo ${INDEX} $ip
+-        let INDEX=${INDEX}+1
+-      done
+-      read -e choose_ip
+-      NODEIP=${NODE_IPS[$choose_ip]}
+-  else
+-    NODEIP=${NODE_IPS[0]}
+-  fi
+-}
+-
+-
+-function compile_error() {
+-if [ "$?" -gt "0" ];
+- then
+-  echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
+-  exit 1
+-fi
+-}
+-
+-function detect_ubuntu() {
+- if [[ $(lsb_release -d) == *16.04* ]]; then
+-   UBUNTU_VERSION=16
+- elif [[ $(lsb_release -d) == *14.04* ]]; then
+-   UBUNTU_VERSION=14
+-else
+-   echo -e "${RED}You are not running Ubuntu 14.04 Installation is cancelled.${NC}"
+-   exit 1
+-fi
+-}
+-
+-
+-function checks() {
+- detect_ubuntu 
+-if [[ $EUID -ne 0 ]]; then
+-   echo -e "${RED}$0 must be run as root.${NC}"
+-   exit 1
+-fi
+-
+-if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "$COIN_DAEMOM" ] ; then
+-  echo -e "${RED}$COIN_NAME is already installed.${NC}"
+-  exit 1
+-fi
+-}
+-
+-function prepare_system() {
+-echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
+-apt-get update >/dev/null 2>&1
+-DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
+-DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
+-apt install -y software-properties-common >/dev/null 2>&1
+-echo -e "${GREEN}Adding bitcoin PPA repository"
+-apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
+-echo -e "Installing required packages, it may take some time to finish.${NC}"
+-apt-get update >/dev/null 2>&1
+-apt-get install libzmq3-dev -y >/dev/null 2>&1
+-apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
+-build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
+-libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
+-libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev>/dev/null 2>&1
+-if [ "$?" -gt "0" ];
+-  then
+-    echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
+-    echo "apt-get update"
+-    echo "apt -y install software-properties-common"
+-    echo "apt-add-repository -y ppa:bitcoin/bitcoin"
+-    echo "apt-get update"
+-    echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
+-libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
+-bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev"
+- exit 1
+-fi
+-
+-clear
+-}
+-
+-
 function important_information() {
  echo
  echo -e "${BLUE}================================================================================================================================${NC}"
@@ -262,30 +309,30 @@ function important_information() {
  echo -e "${BLUE}================================================================================================================================${NC}"
  echo -e "${GREEN}Donations accepted but never required.${NC}"
  echo -e "${BLUE}================================================================================================================================${NC}"
- echo -e "${YELLOW}BCH: qzgnck23pwfag8ucz2f0vf0j5skshtuql5hmwwjhds"
- echo -e "${YELLOW}ETH: 0x765eA1753A1eB7b12500499405e811f4d5164554"
- echo -e "${YELLOW}LTC: LNt9EQputZK8djTSZyR3jE72o7NXNrb4aB${NC}"
+ echo -e "${YELLOW}BTCi: BPfu4MJ84oK5jutAWTx796TEJYzGiyrHCm"
  echo -e "${BLUE}================================================================================================================================${NC}"
 }
-
-function setup_node() {
-  get_ip
-  create_config
-  create_key
-  update_config
-  enable_firewall
-  #install_sentinel
-  important_information
-  configure_systemd
-}
-
-
-##### Main #####
-clear
-
-purgeOldInstallation
-checks
-prepare_system
-download_node
-setup_node
+-
+-function setup_node() {
+-  get_ip
+-  create_config
+-  create_key
+-  update_config
+-  enable_firewall
+-  important_information
+-  if (( $UBUNTU_VERSION == 16 )); then
+-    configure_systemd
+-  else
+-    configure_startup
+-  fi    
+-}
+-
+-
+-##### Main #####
+-clear
+-
+-checks
+-prepare_system
+-compile_node
+-setup_node
 
