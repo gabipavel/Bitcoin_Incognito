dae5049719e125399b9ef1ffd778b29ec90382c4
sub307 <39446798+sub307@users.noreply.github.com>
2018-09-06 23:06:39 +0200

    change copy to move command to avoid duplicates

--- xbi_install.sh
+++ xbi_install.sh
@@ -54,7 +54,7 @@ function download_node() {
   compile_error
   unzip $COIN_ZIP >/dev/null 2>&1
   chmod +x $COIN_DAEMON $COIN_CLI
-  cp $COIN_DAEMON $COIN_CLI $COIN_PATH
+  mv $COIN_DAEMON $COIN_CLI $COIN_PATH
   cd ~ >/dev/null 2>&1
   rm -rf $TMP_FOLDER >/dev/null 2>&1
   clear
